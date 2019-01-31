f74fd9056c560e3c1db54980df7ae87d13977c0b
Gaston Lopez <lopezgaston2011@gmail.com>
2019-01-31 17:31:06 -0300

    Arreglos

--- js/app/bandeja.js
+++ js/app/bandeja.js
@@ -1,17 +1,16 @@
 app.controller('bandejaController', function ($scope)  {
     $scope.Filtro = 0;
-
     $scope.verFiltros = () => {
         switch ($scope.Filtro) {
             case 0:
                 $scope.Filtro = 1;
                 break;
-            case 1:
+            case 1: 
                 $scope.Filtro = 0;
                 break;
         }
     }
 
 
-
+    $scope.Bandeja = [];
 });
